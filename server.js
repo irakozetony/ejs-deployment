0db5c9be2ec56983d34e5498cd89d45ccede81d9
Irakoze Yves <irakozeyves9@gmail.com>
2025-01-21 13:27:05 +0200

    - fix: add app.set("views")

--- server.js
+++ server.js
@@ -48,6 +48,8 @@ createTables()
 
 const app = express()
 
+// Set views directory explicitly
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs")
 app.use(express.urlencoded({extended: false}))
 app.use(express.static("public"))
